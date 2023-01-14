eed41f1531ea5aab4b78558de6a6d4b15ec3a85d
JUSTGHOOST <122677277+JUSTGHOOST@users.noreply.github.com>
2023-01-14 20:19:27 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,27 @@
+from sample_config import Config
+
+class Development(Config):
+
+    APP_ID = 9488183
+
+    API_HASH = "3cc7c94bec23e67b6bf911ed31db5055"
+
+    ALIVE_NAME = "𝙈𝘼𝙃𝙈𝙊𝙐𝘿"
+
+    STRING_SESSION = "1BJWap1sBu50e0k6nX0Xn-B1YzC6lc23Z1xHIQZFXZrBVJ3yWEGfbId81Ogxpkv49myULjKCg9oiLbvGGPvd_AaK15P8u22NTySWgEkokErx_EST3abz8cqvMB95oy1DFOhQjKxAjAn3h6GPgDEuJ5GUrSPgWY0HeN5_XeQ0-ld0XV8cuPo9mfnPtk7yGOhJXZ9W3DVhrFNh9RQQSRxNT-FmhjYC2kuLBp3sSzBFaclCELwmoKP33vgZnlHh1TNI40TlnnW-Qhxlv6vkOham9jC6psg8kplt_fMlFl5LOAjlbYrx24Jv0g6Xa60Bdwue8vqfOgHDjnuWBS1sl8a6pSBP7CMCa-30="
+
+    TG_BOT_TOKEN = "5575942027:AAFKoB70O9X1uN-XzQVNP6Z4LVyLlykxltM"
+
+    TG_BOT_USERNAME = "Mido_Jr7bot"
+    
+    DB_URI = "postgres://fxvjnmoz:WSpt0fDmqM-n44jcjdmHIEQM_TzjFZty@surus.db.elephantsql.com/fxvjnmoz"
+    
+    PRIVATE_GROUP_BOT_API_ID = -1001731138394
+    
+    PM_LOGGER_GROUP_ID = -1001731138394
+
+    COMMAND_HAND_LER = "."
+
+    SUDO_COMMAND_HAND_LER = "."
+
+    TZ = "Africa/Cairo"
